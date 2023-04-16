be5bd8858e97f876ed0f14244af15304e0e75d70
Yariv Kochavi <yariv.kochavi@solaredge.com>
2023-04-16 10:45:42 +0300

    added replaceSubrange to support RangeReplaceableCollection protocol

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -572,3 +572,11 @@ extension ChartDataSet: RangeReplaceableCollection {
         notifyDataSetChanged()
     }
 }
+
+
+extension ChartDataSet{
+    public func replaceSubrange<C>(_ subrange: Swift.Range<Int>, with newElements: C) where C : Collection, ChartDataEntry == C.Element {
+//        entries.replaceSubrange(subrange, with: newElements)
+//        notifyDataSetChanged()
+    }
+}
